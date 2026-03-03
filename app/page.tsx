8fd84252c46f823df5f91a4fbd56ad4c5fa1186d
rafaeldagnoni <rafael.dagnoni@demconsultoria.com>
2026-03-03 14:26:19 -0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: 40}}>
+      <h1>Área do Cliente - D&M Consultoria</h1>
+      <p>Projeto em construção.</p>
+    </main>
+  );
+}
