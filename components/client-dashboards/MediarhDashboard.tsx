409b00e3db6235c12c4c50359752355bf91778ba
rafaeldagnoni <rafael.dagnoni@demconsultoria.com>
2026-03-09 21:39:58 -0300

    Update MediarhDashboard.tsx

--- components/client-dashboards/MediarhDashboard.tsx
+++ components/client-dashboards/MediarhDashboard.tsx
@@ -1,1 +1,10 @@
+"use client";
 
+export default function MediarhDashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Dashboard Mediarh</h1>
+      <p>Este dashboard ainda está em construção.</p>
+    </div>
+  );
+}
