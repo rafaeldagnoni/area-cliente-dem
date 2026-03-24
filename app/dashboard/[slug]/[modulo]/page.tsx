01216d5c64a22a5cc45baf9b44f183ab2de223db
rafaeldagnoni <rafael.dagnoni@demconsultoria.com>
2026-03-23 22:31:35 -0300

    Create page.tsx

---
+++ app/dashboard/[slug]/[modulo]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function DashboardModuloPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Dashboard do Módulo</h1>
+      <p>Você está no dashboard! 🎉</p>
+      {/* Aqui entra seu código do dashboard de Tech4Con */}
+    </div>
+  );
+}
